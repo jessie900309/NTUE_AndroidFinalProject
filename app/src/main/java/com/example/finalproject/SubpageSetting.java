a8b039117f16dc10eb9eda6f7374b2561b0f1f5c
unknown <jessie60125@gmail.com>
2021-12-01 14:32:53 +0800

    TODO scroll color

--- app/src/main/java/com/example/finalproject/SubpageSetting.java
+++ app/src/main/java/com/example/finalproject/SubpageSetting.java
@@ -4,10 +4,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import androidx.fragment.app.Fragment;
 
-public class SubpageSetting extends Fragment {
+public class SubpageSetting extends Fragment
+        implements View.OnClickListener {
 
     public SubpageSetting(){
         // require a empty public constructor
@@ -15,7 +17,44 @@ public class SubpageSetting extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.sub_setting, container, false);
+        View view = inflater.inflate(R.layout.sub_setting, container, false);
+
+        //widget click
+        int[] listenButtonList = {R.id.settings_basic_fixedIncome, R.id.settings_basic_fixedTransfer, R.id.settings_basic_classification, R.id.settings_basic_member,
+                R.id.settings_backup_updateCloud,R.id.settings_backup_outputCSV,R.id.settings_backup_findDev,
+                R.id.setting_help_recommend,R.id.setting_help_aboutApp,R.id.setting_help_acknowledgments};
+        for(int id:listenButtonList){ ((ImageButton)view.findViewById(id)).setOnClickListener(this); }
+
+        return view;
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.settings_basic_fixedIncome:
+                break;
+            case R.id.settings_basic_fixedTransfer:
+                break;
+            case R.id.settings_basic_classification:
+                break;
+            case R.id.settings_basic_member:
+                break;
+            case R.id.settings_backup_updateCloud:
+                break;
+            case R.id.settings_backup_outputCSV:
+                break;
+            case R.id.settings_backup_findDev:
+                break;
+            case R.id.setting_help_recommend:
+                break;
+            case R.id.setting_help_aboutApp:
+                break;
+            case R.id.setting_help_acknowledgments:
+                break;
+            default:
+
+        }
     }
 }
